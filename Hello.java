8cda37cce25b931de9af29fbc35f3c8ef3eb8946
monya-9 <lee99714@naver.com>
2020-04-27 12:37:34 +0900

    asdf

--- Hello.java
+++ Hello.java
@@ -1,7 +1,6 @@
-package com.yedam.prod; // com.yedam.prod.Hello
 
 public class Hello {
-    public static voidmain(String[] args) {
-	    system.out.println('Hello, jhj');
+    public static void voidmain(String[] args) {
+	    System.out.println("Hello, jhj");
 	}
 }
--- VariableExample.java
+++ VariableExample.java
@@ -4,9 +4,9 @@ public class VariableExample {
     public static void main(String[] args) {
 	
 		byte b3 = 126;
-	    for( int i=0l i<100; i++) {
+	    for( int i= 0; i<100; i++); {
 			b3++;
-			System.out.println(b3)
+			System.out.println(b3);
 		}
 	}
 }
